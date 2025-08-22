0b45dd0dbdfd09d1974d1e27e1d0a05328110135
Wero <wero@bluecollarlabs.io>
2025-08-22 12:04:29 -0700

    Add functionality to top buttons
    Added eventListeners and functionality to the
    AC, +/-, and % buttons.  Able to update display with
    button values aswell.

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const rightIcons = ["÷", "x", "-", "+", "="]
 const topIcons = ["AC", "+/-", "%"]
 
 const buttons = document.querySelector(".buttons")
+const display = document.querySelector(".display")
 
 buttonsText.forEach(button => {
     const newButton = document.createElement("button")
@@ -26,5 +27,43 @@ buttonsText.forEach(button => {
         newButton.style.backgroundColor = "#D4D4D2";
         newButton.style.color = "#1C1C1C";
     }
+
+    newButton.addEventListener("click", () => {
+        if(rightIcons.includes(button)) {
+
+        } else if(topIcons.includes(button)) {
+            if(button == "AC") {
+                display.value = "";
+            }else if(button == "+/-") {
+                if(display.value !== ""  && display.value != "0") {
+                    if(display.value[0] == "-") {
+                        display.value = display.value.slice(1);
+                    } else {
+                        display.value = "-" + display.value;
+                    }
+                }
+
+            } else if(button == "%") {
+                display.value = parseFloat(display.value) / 100;
+            }
+            
+        } else {
+            if(button == ".") {
+                if(display.value != "" && !display.value.includes(button)) {
+                    display.value += button
+                }
+            } else if(display.value == "0") {
+                display.value = button;
+            } else {
+                display.value += button;
+            }
+        }
+    })
+
     buttons.appendChild(newButton)
-})
+});
+
+let A = 0;
+let operator = null;
+let B = null;
+
